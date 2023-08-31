62b47a1336a43067a88bd7384c51c0a6f264a73c
Nippylicious <48850817+Nippylicious@users.noreply.github.com>
2023-08-31 18:42:26 +0200

    Update updater.sh

--- updates/updater.sh
+++ updates/updater.sh
@@ -6,7 +6,7 @@ VERSION="31.08.2023"
 # ===========================
 
 rm launcher.sh
-wget http://192.168.191.209:3000/marek/launcher2/raw/branch/main/launcher.sh
+wget https://github.com/Nippylicious/launcher/raw/main/launcher.sh
 chmod +x launcher.sh
 whiptail --msgbox "der Launcher wurde auf Version $VERSION aktualisiert" 8 58
 ./launcher.sh
