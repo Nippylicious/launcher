4e03f8a4a593dd36e4a11b32eb0c9e67213665bc
Nippylicious <48850817+Nippylicious@users.noreply.github.com>
2023-08-31 18:35:49 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -4,4 +4,4 @@
 # 31.08.2023
 # ===========================
 
-bash -c "$(wget -qLO - http://192.168.191.209:3000/marek/launcher2/raw/branch/main/launcher/launcher_menu.sh)"
+bash -c "$(wget -qLO - https://github.com/Nippylicious/launcher/raw/main/launcher/launcher_menu.sh)"
