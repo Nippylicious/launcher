d294e71cc7b8971da68083a94802743a2661c851
Nippylicious <48850817+Nippylicious@users.noreply.github.com>
2023-08-31 18:32:14 +0200

    Add files via upload

---
+++ launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Mareks Launcher
+# 31.08.2023
+# ===========================
+
+bash -c "$(wget -qLO - http://192.168.191.209:3000/marek/launcher2/raw/branch/main/launcher/launcher_menu.sh)"
