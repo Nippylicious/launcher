8b7d1e25e1d8dbab16eeb513851c036996872fbd
Nippylicious <marek@nippys.de>
2024-05-27 17:39:53 +0200

    Timing

--- einstellungen/einstellungen_loeschen.sh
+++ einstellungen/einstellungen_loeschen.sh
@@ -33,7 +33,7 @@ clear
 # Fortschrittsbalken
 {
     for ((i = 0 ; i <= 100 ; i+=5)); do
-        sleep 0.01
+        #sleep 0.1
         echo $i
     done
 } | whiptail --gauge "Einstellungen werden zurueckgesetzt..." 6 50 0
